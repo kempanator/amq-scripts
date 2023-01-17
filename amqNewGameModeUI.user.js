ba90f7107f1e8e9ab76c3827aee56f2a642b05ae
kempanator <1497607+kempanator@users.noreply.github.com>
2023-01-16 21:01:24 -0600

    update

--- amqNewGameModeUI.user.js
+++ amqNewGameModeUI.user.js
@@ -35,8 +35,7 @@ let autothrowCount = false;
 $("#qpOptionContainer").width($("#qpOptionContainer").width() + 35);
 $("#qpOptionContainer > div").append($(`<div id="qpNGM" class="clickAble qpOption"><img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAMAAAAM7l6QAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAGUExURdnZ2QAAAE/vHxMAAAACdFJOU/8A5bcwSgAAAAlwSFlzAAAOwwAADsMBx2+oZAAAAFdJREFUOE/VjsENwCAMA8n+S4NN09qI9lOBxL0cXxSlxCen69JgGFmsoRIUymoNGGds0K+41iuMqiFZ5mCajuSgmoWBsrt73W+4flIOqq8km/rajD86ogIU2QKttGjahwAAAABJRU5ErkJggg=='/></div>`)
     .click(() => {
-        if (ngmWindow.isVisible()) ngmWindow.close();
-        else ngmWindow.open();
+        ngmWindow.isVisible() ? ngmWindow.close() : ngmWindow.open();
     })
     .popover({
         content: "New Game Mode UI",
