1e88208c3d1a52a7267cdb4bb529c21270a13bfe
kempanator <1497607+kempanator@users.noreply.github.com>
2023-01-24 20:24:33 -0600

    update

--- amqAnswerStats.user.js
+++ amqAnswerStats.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ Answer Stats
 // @namespace    https://github.com/kempanator
-// @version      0.3
+// @version      0.4
 // @description  Adds a window to display quiz answer stats
 // @author       kempanator
 // @match        https://animemusicquiz.com/*
@@ -17,6 +17,7 @@
 Features:
 1. Add a window with answer stats
 2. Add a window for anisongdb search results
+3. Add a window to track average guess time for players' correct answers
 2. Add anisongdb anime and artist lookup buttons to song info
 3. Add anilist, kitsu, myanimelist, annid, 720, 480, mp3 links to song info
 4. Extra song info button is always visible
@@ -31,11 +32,14 @@ let loadInterval = setInterval(() => {
     }
 }, 500);
 
-const version = "0.3";
+const version = "0.4";
 let answerStatsWindow;
+let answerTimeTrackWindow;
 let anisongdbWindow;
 let answers = {}; //{1: {name, answer, correct}, ...}
+let timeTrack = {}; //{1: {name, averageTime, totalTime, totalCorrect}, ...}
 let listLowerCase = [];
+let timeTrackButton = true;
 let anisongdbSearchButtons = true;
 let allSourceLinks = true;
 let animeSortAscending = false;
@@ -43,12 +47,13 @@ let artistSortAscending = false;
 let songSortAscending = false;
 let typeSortAscending = false;
 let vintageSortAscending = false;
+let timeTrackSort = "time"; //"time" "name" "score"
+let timeTrackSortAscending = false;
 
 $("#qpOptionContainer").width($("#qpOptionContainer").width() + 35);
 $("#qpOptionContainer > div").append($(`<div id="qpAnswerStats" class="clickAble qpOption"><i aria-hidden="true" class="fa fa-list-alt qpMenuItem"></i></div>`)
     .click(() => {
-        if (answerStatsWindow.isVisible()) answerStatsWindow.close();
-        else answerStatsWindow.open();
+        answerStatsWindow.isVisible() ? answerStatsWindow.close() : answerStatsWindow.open();
     })
     .popover({
         content: "Answer Stats",
@@ -70,15 +75,24 @@ function setup() {
             setTimeout(() => { gameChat.systemMessage("Answer Stats - " + version) }, 1);
         }
     }).bindListener();
-    new Listener("get all song names", (payload) => {
+    new Listener("get all song names", () => {
         setTimeout(() => {
             listLowerCase = quiz.answerInput.autoCompleteController.list.map(x => x.toLowerCase());
         }, 1);
     }).bindListener();
-    new Listener("update all song names", (payload) => {
+    new Listener("update all song names", () => {
         setTimeout(() => {
             listLowerCase = quiz.answerInput.autoCompleteController.list.map(x => x.toLowerCase());
         }, 1);
+    }).bindListener();
+    new Listener("Game Starting", () => {
+        timeTrack = {};
+    }).bindListener();
+    new Listener("Join Game", () => {
+        timeTrack = {};
+    }).bindListener();
+    new Listener("Spectate Game", () => {
+        timeTrack = {};
     }).bindListener();
     new Listener("player answers", (payload) => {
         //console.log(payload);
@@ -104,7 +118,30 @@ function setup() {
             let quizPlayer = answers[player.gamePlayerId];
             if (quizPlayer) {
                 quizPlayer.correct = player.correct;
-                if (player.correct) correctPlayers[answers[player.gamePlayerId].name] = amqAnswerTimesUtility.playerTimes[player.gamePlayerId];
+                if (player.correct) {
+                    let speed = amqAnswerTimesUtility.playerTimes[player.gamePlayerId]
+                    correctPlayers[answers[player.gamePlayerId].name] = speed;
+                    if (speed && speed < 100000) {
+                        timeObject = timeTrack[player.gamePlayerId];
+                        if (timeObject) {
+                            timeTrack[player.gamePlayerId] = {
+                                name: quizPlayer.name,
+                                averageTime: (timeObject.totalTime + speed) / (timeObject.totalCorrect + 1),
+                                totalTime: timeObject.totalTime + speed,
+                                totalCorrect: timeObject.totalCorrect + 1
+                            };
+                        }
+                        else {
+                            timeTrack[player.gamePlayerId] = {
+                                name: quizPlayer.name,
+                                averageTime: speed,
+                                totalTime: speed,
+                                totalCorrect: 1
+                            };
+                        }
+                        //console.log(timeTrack[player.gamePlayerId])
+                    }
+                }
             }
             else {
                 console.log("player id not found: " + player.gamePlayerId);
@@ -152,7 +189,6 @@ function setup() {
                         <span><b>${rankedText()}</b></span>
                         <span style="margin-left: 20px"><b>Song:</b> ${quiz.infoContainer.$currentSongCount.text()}/${quiz.infoContainer.$totalSongCount.text()}</span>
                         <span style="margin-left: 20px"><b>Total Players:</b> ${totalPlayers}</span>
-                    </div>
                 `);
             }
             answerStatsWindow.panels[0].panel.append(`
@@ -200,6 +236,10 @@ function setup() {
             }
             answerStatsWindow.panels[0].panel.append($ulCorrect);
             answerStatsWindow.panels[0].panel.append($ulWrong);
+
+            if (timeTrackButton) {
+                displayTimeTrackResults();
+            }
 
             if (anisongdbSearchButtons) {
                 $("#answerStatsAnisongdbSearchRow").remove();
@@ -261,12 +301,30 @@ function setup() {
         height: 300,
         minWidth: 0,
         minHeight: 0,
-        zIndex: 1070,
+        zIndex: 1001,
         resizable: true,
         draggable: true
     });
     answerStatsWindow.addPanel({
         id: "answerStatsPanel",
+        width: 1.0,
+        height: "100%",
+        scrollable: {x: false, y: true}
+    });
+
+    answerTimeTrackWindow = new AMQWindow({
+        id: "answerTimeTrackWindow",
+        title: "Time Track",
+        width: 200,
+        height: 300,
+        minWidth: 0,
+        minHeight: 0,
+        zIndex: 1002,
+        resizable: true,
+        draggable: true
+    });
+    answerTimeTrackWindow.addPanel({
+        id: "answerTimeTrackPanel",
         width: 1.0,
         height: "100%",
         scrollable: {x: false, y: true}
@@ -279,7 +337,7 @@ function setup() {
         height: 400,
         minWidth: 0,
         minHeight: 0,
-        zIndex: 1100,
+        zIndex: 1003,
         resizable: true,
         draggable: true
     });
@@ -329,6 +387,33 @@ function setup() {
         }
     });
     $("#anisongdbSearchPartialCheckbox").prop("checked", true);
+
+    if (timeTrackButton) {
+        answerStatsWindow.window.find(".modal-header").prepend($(`<button id="answerStatsTimeTrackButton">time<br>track</button>`).click(() => {
+            answerTimeTrackWindow.isVisible() ? answerTimeTrackWindow.close() : answerTimeTrackWindow.open();
+        }));
+        answerStatsWindow.window.find("#answerStatsTimeTrackButton").popover({
+            container: "#gameContainer",
+            placement: "top",
+            trigger: "hover",
+            content: "track average guess time of all players' correct answers"
+        });
+        let $button1 = $(`<button id="timeTrackSortModeButton">${timeTrackSort}</button>`).click(function() {
+            if (timeTrackSort === "time") timeTrackSort = "name";
+            else if (timeTrackSort === "name") timeTrackSort = "score";
+            else if (timeTrackSort === "score") timeTrackSort = "time";
+            $(this).text(timeTrackSort);
+            displayTimeTrackResults();
+        });
+        let $button2 = $(`<button id="timeTrackSortDirectionButton">${timeTrackSortAscending ? "ascend" : "descend"}</button>`).click(function() {
+            timeTrackSortAscending = !timeTrackSortAscending;
+            $(this).text(timeTrackSortAscending ? "ascend" : "descend");
+            displayTimeTrackResults();
+        });
+        answerTimeTrackWindow.window.find(".modal-header h2").remove();
+        answerTimeTrackWindow.window.find(".modal-header").append($button1);
+        answerTimeTrackWindow.window.find(".modal-header").append($button2);
+    }
 
     AMQ_addScriptData({
         name: "Answer Stats",
@@ -441,6 +526,36 @@ function getAnisongdbData(mode, query, partial) {
     });
 }
 
+// display average time list in time track window
+function displayTimeTrackResults() {
+    answerTimeTrackWindow.panels[0].clear();
+    let sortedTimeTrackIds;
+    if (timeTrackSort === "time") {
+        sortedTimeTrackIds = timeTrackSortAscending
+            ? Object.keys(timeTrack).sort((a, b) => timeTrack[a].averageTime - timeTrack[b].averageTime)
+            : Object.keys(timeTrack).sort((a, b) => timeTrack[b].averageTime - timeTrack[a].averageTime);
+    }
+    else if (timeTrackSort === "name") {
+        sortedTimeTrackIds = timeTrackSortAscending
+            ? Object.keys(timeTrack).sort((a, b) => timeTrack[a].name.localeCompare(timeTrack[b].name))
+            : Object.keys(timeTrack).sort((a, b) => timeTrack[b].name.localeCompare(timeTrack[a].name));
+    }
+    else if (timeTrackSort === "score") {
+        sortedTimeTrackIds = timeTrackSortAscending
+            ? Object.keys(timeTrack).sort((a, b) => timeTrack[a].totalCorrect - timeTrack[b].totalCorrect)
+            : Object.keys(timeTrack).sort((a, b) => timeTrack[b].totalCorrect - timeTrack[a].totalCorrect);
+    }
+    for (let id of sortedTimeTrackIds) {
+        answerTimeTrackWindow.panels[0].panel.append(`
+            <div>
+                <span class="trackTime">${Math.round(timeTrack[id].averageTime)}ms</span>
+                <span class="trackName">${timeTrack[id].name}</span>
+                <span class="trackScore">${timeTrack[id].totalCorrect}</span>
+            </div>
+        `);
+    }
+}
+
 // input table element, column name, and sort ascending boolean
 function sortAnisongdbTableEntries($table, column, sortAscending) {
     let sortedElements = sortAscending
@@ -540,7 +655,7 @@ function applyStyles() {
             user-select: none;
         }
         #anisongdbTable tr.bodyRow:hover {
-            color: #70b7ff;
+            color: #70B7FF;
         }
         #anisongdbTable .anime {
             width: 25%;
@@ -556,6 +671,49 @@ function applyStyles() {
         }
         #anisongdbTable .vintage {
             width: 15%;
+        }
+        #answerStatsTimeTrackButton {
+            background: none;
+            border: 1px solid #D9D9D9;
+            border-radius: 4px;
+            color: #D9D9D9;
+            line-height: 1;
+            font-weight: bold;
+            padding: 5px 5px;
+            position: absolute;
+            float: left;
+            user-select: none;
+        }
+        #answerStatsTimeTrackButton:hover {
+            opacity: .8;
+        }
+        #answerTimeTrackWindow .modal-header {
+            padding: 0;
+        }
+        #answerTimeTrackWindow .modal-header button {
+            background: none;
+            border: 1px solid #D9D9D9;
+            border-radius: 4px;
+            color: #D9D9D9;
+            font-weight: bold;
+            margin: 5px 0px 5px 5px;
+            padding: 0px 5px;
+            user-select: none;
+        }
+        #answerTimeTrackWindow .modal-header button:hover {
+            opacity: .8;
+        }
+        #answerTimeTrackWindow span.trackTime {
+            width: 70px;
+            display: inline-block;
+        }
+        #answerTimeTrackWindow span.trackName {
+            display: inline-block;
+        }
+        #answerTimeTrackWindow span.trackScore {
+            opacity: .7;
+            margin-left: 8px;
+            display: inline-block;
         }
     `));
     document.head.appendChild(style);
