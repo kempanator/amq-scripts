c5d910d46b1a5d1926b32bc20cdf906360c357c3
kempanator <1497607+kempanator@users.noreply.github.com>
2025-05-08 10:46:38 -0500

    update

--- amqShowAllSongLinks.user.js
+++ amqShowAllSongLinks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AMQ Show All Song Links
 // @namespace    https://github.com/kempanator
-// @version      0.8
+// @version      0.9
 // @description  Show all song links in the song info container
 // @author       kempanator
 // @match        https://*.animemusicquiz.com/*
@@ -20,19 +20,19 @@ let loadInterval = setInterval(() => {
     }
 }, 500);
 
-const version = "0.8";
+const version = "0.9";
 
 function setup() {
-    new Listener("answer results", (payload) => {
+    new Listener("answer results", (data) => {
         setTimeout(() => {
             $("#qpSongInfoLinkRow b").remove();
-            let urlAni = payload.songInfo.siteIds.aniListId ? "https://anilist.co/anime/" + payload.songInfo.siteIds.aniListId : "";
-            let urlKit = payload.songInfo.siteIds.kitsuId ? "https://kitsu.io/anime/" + payload.songInfo.siteIds.kitsuId : "";
-            let urlMal = payload.songInfo.siteIds.malId ? "https://myanimelist.net/anime/" + payload.songInfo.siteIds.malId : "";
-            let urlAnn = payload.songInfo.siteIds.annId ? "https://www.animenewsnetwork.com/encyclopedia/anime.php?id=" + payload.songInfo.siteIds.annId : "";
-            let url720 = formatURL(payload.songInfo.videoTargetMap.catbox?.[720] ?? payload.songInfo.videoTargetMap.openingsmoe?.[720]);
-            let url480 = formatURL(payload.songInfo.videoTargetMap.catbox?.[480] ?? payload.songInfo.videoTargetMap.openingsmoe?.[480]);
-            let urlmp3 = formatURL(payload.songInfo.videoTargetMap.catbox?.[0] ?? payload.songInfo.videoTargetMap.openingsmoe?.[0]);
+            let urlAni = data.songInfo.siteIds.aniListId ? "https://anilist.co/anime/" + data.songInfo.siteIds.aniListId : "";
+            let urlKit = data.songInfo.siteIds.kitsuId ? "https://kitsu.io/anime/" + data.songInfo.siteIds.kitsuId : "";
+            let urlMal = data.songInfo.siteIds.malId ? "https://myanimelist.net/anime/" + data.songInfo.siteIds.malId : "";
+            let urlAnn = data.songInfo.siteIds.annId ? "https://www.animenewsnetwork.com/encyclopedia/anime.php?id=" + data.songInfo.siteIds.annId : "";
+            let url720 = formatURL(data.songInfo.videoTargetMap.catbox?.[720] ?? data.songInfo.videoTargetMap.openingsmoe?.[720]);
+            let url480 = formatURL(data.songInfo.videoTargetMap.catbox?.[480] ?? data.songInfo.videoTargetMap.openingsmoe?.[480]);
+            let urlMp3 = formatURL(data.songInfo.videoTargetMap.catbox?.[0] ?? data.songInfo.videoTargetMap.openingsmoe?.[0]);
             let $b = $(`<b></b>`);
             $b.append($("<a></a>").attr({href: urlAni, target: "_blank"}).addClass(urlAni ? "" : "disabled").text("ANI"));
             $b.append($("<a></a>").attr({href: urlKit, target: "_blank"}).addClass(urlKit ? "" : "disabled").text("KIT"));
@@ -41,7 +41,7 @@ function setup() {
             $b.append("<br>");
             $b.append($("<a></a>").attr({href: url720, target: "_blank"}).addClass(url720 ? "" : "disabled").text("720"));
             $b.append($("<a></a>").attr({href: url480, target: "_blank"}).addClass(url480 ? "" : "disabled").text("480"));
-            $b.append($("<a></a>").attr({href: urlmp3, target: "_blank"}).addClass(urlmp3 ? "" : "disabled").text("MP3"));
+            $b.append($("<a></a>").attr({href: urlMp3, target: "_blank"}).addClass(urlMp3 ? "" : "disabled").text("MP3"));
             $("#qpSongInfoLinkRow").prepend($b);
         }, 0);
     }).bindListener();
@@ -68,24 +68,22 @@ function formatURL(url) {
             return videoResolver.formatUrl(url);
         }
     }
-    else {
-        return "";
-    }
+    return "";
 }
 
 // apply styles
 function applyStyles() {
     //$("#showAllSongLinksStyle").remove();
-    let style = document.createElement("style");
-    style.type = "text/css";
-    style.id = "showAllSongLinksStyle";
-    style.appendChild(document.createTextNode(`
+    let css = /*css*/ `
         #qpSongInfoLinkRow b a {
             margin: 0 3px;
         }
         #qpExtraSongInfo {
             z-index: 1;
         }
-    `));
+    `;
+    let style = document.createElement("style");
+    style.id = "showAllSongLinksStyle";
+    style.textContent = css.trim();
     document.head.appendChild(style);
 }
